1f41c3130b167de5f96ef13a407aef003c3ac8df
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-05 11:16:02 +0600

    added date fns pakage and ui interface new date

--- src/components/Header.jsx/Header.jsx
+++ src/components/Header.jsx/Header.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
+import { format } from 'date-fns';
 
 const Header = () => {
     return (
         <div className='flex flex-col justify-center gap-3 items-center mt-5'>
            <img className='w-[350px]' src={logo} alt="" />
            <p>Journalism Without Fear or Favour</p>
-           <button className='btn btn-primary'>check</button>
+           <p>
+            {format(new Date(), 'EEEE, 	MMMM MM, yyyy')}
+           </p>
         </div>
     );
 };
