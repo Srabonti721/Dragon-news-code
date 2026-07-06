217a7b0a965ab03b87fee62a03fe1ced1d1480d2
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-06 08:27:43 +0600

    added page layout and load categories dato use hook and suspance

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,19 +1,23 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home";
 import ErrorPage from "../pages/ErrorPage";
+import HomePage from "../pages/HomePage";
+import Category from "../pages/Category";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component:Home,
-  },
-  {
-    path: "/auth",
-    element:<div>auth</div>,
-  },
-  {
-    path: "/",
-    Component:Home,
+    children:[
+      {
+        path:'',
+        element:<HomePage></HomePage>
+      },
+      {
+        path:'/category/:id',
+        element:<Category></Category>
+      }
+    ]
   },
   {
     path: "*",
