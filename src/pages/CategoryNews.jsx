d7e4511020f6f63bc2e412ee3ffd1a6b2b23ab40
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-07 00:08:28 +0600

    filter data and useEffect hook

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,12 +1,32 @@
-import React, {} from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
+    const [categoryNews, setCategoryNews] = useState([]);
+    const { id } = useParams();
+    const newsID = parseInt(id)
+    const data = useLoaderData();
 
-    
+    useEffect(() => {
+        if (id == 0) {
+            setCategoryNews(data);
+            return
+        }
+        else if (id == 1) {
+            const filterNews = data.filter((news) => news.others.is_today_pick ===  true)
+            setCategoryNews(filterNews)
+        }
+        else {
+            const filterNews = data.filter((news) => news.category_id === newsID)
+            console.log(filterNews);
+            setCategoryNews(filterNews)
+        }
+
+    }, [data, id])
     return (
         <div>
-<h2>categorey news</h2>
+            <h2>total news {categoryNews.length} found</h2>
+
         </div>
     );
 };
